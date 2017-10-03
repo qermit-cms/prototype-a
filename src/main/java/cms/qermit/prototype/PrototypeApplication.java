2159aaab7e8e58780ff8bbee6e06e6164d364b1a
qermit <qermit.cms@gmail.com>
2017-10-03 23:19:56 +0100

    Added Application class

---
+++ src/main/java/cms/qermit/prototype/PrototypeApplication.java
@@ -0,0 +1,12 @@
+package cms.qermit.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(PrototypeApplication.class, args);
+    }
+}
