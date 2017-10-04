7c19642fe111b98d276c0ea8de99e56c4f089033
qermit <qermit.cms@gmail.com>
2017-10-05 00:59:51 +0100

    Remove unused dependency in application class

--- src/main/java/cms/qermit/prototype/PrototypeApplication.java
+++ src/main/java/cms/qermit/prototype/PrototypeApplication.java
@@ -1,16 +1,11 @@
 package cms.qermit.prototype;
 
-import cms.qermit.prototype.mongo.TokenRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PrototypeApplication {
-
-    @Autowired
-    private TokenRepository tokenRepository;
-
+    
     public static void main(String[] args) throws Exception {
         SpringApplication.run(PrototypeApplication.class, args);
     }
